ffb818e544d7c171ea2fe648af4ce92d1f4f21d2
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-04-14 13:01:55 +0500

    navbar component added

---
+++ resources/js/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import NavLink from '@/Components/NavLink';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import { Link, usePage } from '@inertiajs/react';
+import { useState } from 'react';
+
+export default function NavBar() {
+    return (
+        <>
+             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <NavLink
+                    href={route('dashboard')}
+                    active={route().current('dashboard')}
+                >
+                    Dashboard
+                </NavLink>
+            </div>
+            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <NavLink
+                    href={route('users.index')}
+                    active={route().current('users')}
+                >
+                    User
+                </NavLink>
+            </div>
+        </>
+    )
+}
