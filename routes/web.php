f54ddda4b3db083a27e75eccf89c1748ef9610a6
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-04-28 16:07:03 +0500

    changing dashboard route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth','verified'])->group(function(){
+    Route::get('/dashboard',fn()=> Inertia::render('dashboard'))->name('dashboard');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
