98db2d1690ae227273ee8262582ecd8c5bfc15c0
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-04-30 17:16:37 +0500

    adding relations

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,18 @@ class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id');
+    }
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
