a91f6124b8a7dfedea59a6f7f2e39c7a6ce60d11
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-04-30 15:34:19 +0500

    tablehead th component

--- resources/js/Components/TableHeading.jsx
+++ resources/js/Components/TableHeading.jsx
@@ -1,13 +1,17 @@
-export default function TableHeading({ name, sortable = true, sortField, sortOrder }) {
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/16/solid'
+
+export default function TableHeading({ name, sortable = true, sortField, sortOrder, children,sortChanged = () => {} }) {
     return (
         <>
-            <th onClick={(e) => sortChanged('id')} className="px-3 py-3">
+            <th onClick={(e) => sortChanged(name)} className="px-3 py-3">
                 <div className="flex items-center justify-between">
-                    ID
-                    <div>
-                        <ChevronUpIcon className="w-4" />
-                        <ChevronDownIcon className="w-4" />
-                    </div>
+                    {children}
+                    {sortable && (
+                        <div>
+                            <ChevronUpIcon className="w-4" />
+                            <ChevronDownIcon className="w-4" />
+                        </div>
+                    )}
                 </div>
             </th>
         </>
