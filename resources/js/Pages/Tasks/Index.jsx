8c200ea9e0b192c1f8923f5c4041b9fb4445022d
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-05 16:51:37 +0500

    updating tasks listing

--- resources/js/Pages/Tasks/Index.jsx
+++ resources/js/Pages/Tasks/Index.jsx
@@ -1,49 +1,8 @@
-import Pagination from "@/Components/Pagination";
-import SelectInput from "@/Components/SelectInput";
-import TableHeading from "@/Components/TableHeading";
-import TextInput from "@/Components/TextInput";
-import { TASK_PRIORITY_CLASS_MAP, TASK_PRIORITY_TEXT_MAP, TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/Constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link, router } from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
 import TaskTable from "./TaskTable";
 
-
 export default function Index({ tasks, queryParams = null }) {
-
-    queryParams = queryParams || {};
-
-    const searchFieldChanged = (name, value) => {
-        console.log(name, value);
-        if (value) {
-            queryParams[name] = value;
-        } else {
-            delete queryParams[name];
-        }
-        console.log(queryParams);
-        router.get(route('tasks.index'), queryParams);
-    }
-
-    const onKeyPress = (name, e) => {
-        if (e.key !== 'Enter') return;
-        searchFieldChanged(name, e.target.value);
-    }
-
-
-    const sortChanged = (name) => {
-        if (name === queryParams.sortField) {
-            if (queryParams.sortOrder === 'asc') {
-                queryParams.sortOrder = 'desc';
-            } else {
-                queryParams.sortOrder = 'asc';
-            }
-        } else {
-            queryParams.sortField = name;
-            queryParams.sortOrder = 'asc';
-        }
-        console.log(queryParams);
-        router.get(route('tasks.index'), queryParams);
-
-    }
 
     return (
         <AuthenticatedLayout
@@ -59,7 +18,7 @@ export default function Index({ tasks, queryParams = null }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <TaskTable tasks={tasks} queryParams={queryParams} sortChanged={sortChanged}/>
+                            <TaskTable tasks={tasks} queryParams={queryParams}/>
                         </div>
                     </div>
                 </div>
