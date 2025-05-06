06cf4f9654f460128126365c09630d6a5a14712b
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-06 11:18:13 +0500

    Create Project page

--- resources/js/Pages/Projects/Create.jsx
+++ resources/js/Pages/Projects/Create.jsx
@@ -1,7 +1,26 @@
-export function Create(){
-    return (
-        <>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
-        </>
+export default function Create() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Add Project
+                </h2>
+            }
+        >
+            <Head title="Add Projects" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Form goes here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
     )
 }
