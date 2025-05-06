1559aaf62737512ffb96709bacbed1b7ef4a3c56
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-06 11:42:50 +0500

    updating breadcrumb component

--- resources/js/Components/BreadCrumb.jsx
+++ resources/js/Components/BreadCrumb.jsx
@@ -1,9 +1,18 @@
-export function BreadCrumb({children})
-{
+import { HomeIcon } from "@heroicons/react/16/solid";
+import { Link } from "@inertiajs/react";
+
+export function BreadCrumb({ children }) {
     return (
         <>
             <nav className="mb-4 text-sm text-gray-600 dark:text-gray-300" aria-label="Breadcrumb">
                 <ol className="flex items-center space-x-2">
+                    <li className="flex items-center space-x-1">
+                        <Link href={route('dashboard')} className="flex items-center text-blue-600 hover:underline dark:text-blue-400">
+                            <HomeIcon className="w-4 h-4 mr-1" />
+                            <span>Dashboard</span>
+                        </Link>
+                        <span className="mx-2 text-gray-400">/</span>
+                    </li>
                     {children}
                 </ol>
             </nav>
