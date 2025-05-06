2b0ba3864afca8ec08a4bb03bfff3082475dc501
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-06 10:58:42 +0500

    setting project column to false

--- resources/js/Pages/Projects/Show.jsx
+++ resources/js/Pages/Projects/Show.jsx
@@ -99,7 +99,7 @@ export default function Show({ auth, project,tasks,queryParams }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="text-gray-900 dark:text-gray-100">
-                            <TaskTable tasks={tasks} queryParams={queryParams}/>
+                            <TaskTable tasks={tasks} queryParams={queryParams} showProject={false}/>
                         </div>
                     </div>
                 </div>
