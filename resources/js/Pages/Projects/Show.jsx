139c25e339274a523d5142a2b15879b497087012
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-06 16:06:35 +0500

    fixed task filtration issue

--- resources/js/Pages/Projects/Show.jsx
+++ resources/js/Pages/Projects/Show.jsx
@@ -111,7 +111,7 @@ export default function Show({ auth, project, tasks, queryParams }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="text-gray-900 dark:text-gray-100">
-                            <TaskTable tasks={tasks} queryParams={queryParams} showProject={false} />
+                            <TaskTable tasks={tasks} queryParams={queryParams} showProject={false} project={project.id} />
                         </div>
                     </div>
                 </div>
