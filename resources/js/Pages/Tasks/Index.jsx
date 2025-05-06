f6f8062e09426003c25e92cf313703675fe1dee6
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-06 10:38:20 +0500

    removing padding from table sections

--- resources/js/Pages/Tasks/Index.jsx
+++ resources/js/Pages/Tasks/Index.jsx
@@ -17,7 +17,7 @@ export default function Index({ tasks, queryParams = null }) {
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                        <div className="text-gray-900 dark:text-gray-100">
                             <TaskTable tasks={tasks} queryParams={queryParams}/>
                         </div>
                     </div>
