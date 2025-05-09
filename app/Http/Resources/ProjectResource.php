798ade3fdcc0b57c013b75c92b432602a43a303c
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-09 09:05:08 +0500

    project resource

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectResource extends JsonResource
 {
@@ -21,7 +22,7 @@ class ProjectResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'image_path' => $this->image_path,
+            'image_path' => $this->image_path ? Storage::url($this->image_path) : 'https://placehold.co/600x400?text=Hello\nWorld' ,
             'description' => $this->description,
             'status' => $this->status,
             'created_by' =>  new UserResource($this->createdBy),
