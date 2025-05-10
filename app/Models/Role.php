66042c271e41dcf3dd2d5dec2bd10576e5533d6a
Abdul Muqtadir <129877143+Muqtadir44@users.noreply.github.com>
2025-05-10 17:20:41 +0500

    role model
    - adding active fetch method
    - user relation

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $guarded = [];
+
+
+    static function Active()
+    {
+        return Self::whereStatus(1)->get();
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'role_id');
+    }
 }
