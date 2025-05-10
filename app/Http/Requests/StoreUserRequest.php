baa803174384154ebd478b7e7da03456edd26489
Abdul Muqtadir <129877143+Muqtadir44@users.noreply.github.com>
2025-05-10 17:17:31 +0500

    adding validation in user store request

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreUserRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,13 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'email' => ['required','email'],
+            'password' => ['required','string','min:8'],
+            'status'=> ['required',Rule::in(['1','0'])],
+            'role' => ['required', Rule::exists('roles', 'id')],
+            'designation' => ['required', Rule::exists('designations', 'id')],
+            'picture' => ['nullable','image','mimes:jpeg,png,jpg','max:2048'],
         ];
     }
 }
