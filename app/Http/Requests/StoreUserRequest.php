15fe0035f5d9f8f2a38fada767408b69f85cc9cf
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-16 10:02:09 +0500

    adding unique email rule

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -24,7 +24,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => ['required','string','max:255'],
-            'email' => ['required','email'],
+            'email' => ['required','email',Rule::unique('users', 'email')],
             'password' => ['required','string','min:8'],
             'status'=> ['required',Rule::in(['1','0'])],
             'role' => ['required', Rule::exists('roles', 'id')],
