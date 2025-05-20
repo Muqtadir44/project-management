0247508e2c7a603d5f2e40ecc63e4243cd5fad23
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-20 12:31:06 +0500

    adding picture in resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -26,6 +27,7 @@ class UserResource extends JsonResource
             'designation' => new DesignationResource($this->designation),
             'role'=> new RoleResource($this->role),
             'created_at' => (new Carbon($this->created_at))->format('d-M-Y'),
+            "picture" => $this->picture ? Storage::url($this->picture) : 'https://placehold.co/600x400?text=Hello\nWorld' ,
         ];
     }
 }
