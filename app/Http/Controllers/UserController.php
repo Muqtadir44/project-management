8ebd675d9e5a587b18a8d2149174b70524fb5ef9
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-05-20 12:51:31 +0500

    updated controller destory method and update method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,6 @@ use App\Models\Designation;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
@@ -29,6 +28,10 @@ class UserController extends Controller
             $query->where("name", "like", "%" . request("name") . "%");
         }
 
+        if (request("email")) {
+            $query->where("email", "like", "%" . request("email") . "%");
+        }
+
         // if(request("status")){
         //     $query->where("status", request("status"));
         // }
@@ -40,7 +43,7 @@ class UserController extends Controller
         // dd($users);
 
         return Inertia::render('Users/Index', props: [
-            'users' => UserResource::collection($users),
+            'users'       => UserResource::collection($users),
             "queryParams" => request()->query() ?: null,
         ]);
     }
@@ -51,9 +54,8 @@ class UserController extends Controller
     public function create()
     {
 
-        $roles = Role::Active();
+        $roles        = Role::Active();
         $designations = Designation::Active();
-
 
         return Inertia::render('Users/Create', ['roles' => $roles, 'designations' => $designations]);
     }
@@ -72,15 +74,14 @@ class UserController extends Controller
             $picture = $picture->store('users', 'public');
         }
 
-
         User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-            'status' => $data['status'],
-            'role_id' => $data['role'],
+            'name'           => $data['name'],
+            'email'          => $data['email'],
+            'password'       => bcrypt($data['password']),
+            'status'         => $data['status'],
+            'role_id'        => $data['role'],
             'designation_id' => $data['designation'],
-            'picture' => $picture
+            'picture'        => $picture,
         ]);
 
         return redirect()->route('users.index')->with('success', 'User created successfully.');
@@ -100,15 +101,14 @@ class UserController extends Controller
     public function edit(string $id)
     {
 
-        $roles = Role::get();
+        $roles        = Role::get();
         $designations = Designation::get();
-
 
         $user = User::findOrFail($id);
         return Inertia::render('Users/Edit', props: [
-            'user' => $user,
-            'roles' => $roles,
-            'designations' => $designations
+            'user'         => $user,
+            'roles'        => $roles,
+            'designations' => $designations,
         ]);
 
     }
@@ -122,35 +122,26 @@ class UserController extends Controller
 
         // Handle Picture
         if (isset($data['picture']) && $data['picture']) {
-            if ($user->image_path && Storage::disk('public')->exists($user->image_path)) {
-                Storage::disk('public')->delete($user->image_path);
+            if ($user->picture && Storage::disk('public')->exists($user->picture)) {
+                Storage::disk('public')->delete($user->picture);
             }
             $picture = $data['picture']->store('users', 'public');
         } else {
             $picture = $user->picture;
         }
 
-        // Handle Password
-        // if (isset($data['password']) && $data['password']) {
-        //     $password = bcrypt($data['password']);
-        // } else {
-        //     $password = $user->password; // keep existing password
-        // }
-
-
         $user->update([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => $data['password'] ?? bcrypt($data['password']),
-            'status' => $data['status'],
-            'role_id' => $data['role'],
+            'name'           => $data['name'],
+            'email'          => $data['email'],
+            'password'       => $data['password'] ?? bcrypt($data['password']),
+            'status'         => $data['status'],
+            'role_id'        => $data['role'],
             'designation_id' => $data['designation'],
-            'picture' => $picture
+            'picture'        => $picture,
         ]);
 
         return redirect()->route('users.index')->with('success', 'User updated successfully');
     }
-
 
     /**
      * Remove the specified resource from storage.
@@ -158,6 +149,9 @@ class UserController extends Controller
     public function destroy(string $id)
     {
         $user = User::findOrFail($id);
+        if ($user->picture && Storage::disk('public')->exists($user->picture)) {
+            Storage::disk('public')->delete($user->picture);
+        }
         $user->delete();
         return redirect()->route('users.index')->with('success', 'User deleted.');
     }
