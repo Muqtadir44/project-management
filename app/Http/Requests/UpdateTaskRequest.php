acdad6021c6b2cb872ae0c2fcaeef124d9834c0f
Abdul Muqtadir <129877143+Muqtadir44@users.noreply.github.com>
2025-06-13 23:48:52 +0500

    adding update task request

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateTaskRequest extends FormRequest
 {
@@ -22,7 +23,13 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'project'=>['required','exists:projects,id'],
+            'description' => ['string'],
+            'due_date' => ['nullable','date','after_or_equal:today'],
+            'status'=> ['required',Rule::in(['pending','in_progress','completed'])],
+            'priority'=> ['required',Rule::in(['low','medium','high'])],
+            'image' => ['nullable','image','mimes:jpeg,png,jpg','max:2048'],
         ];
     }
 }
