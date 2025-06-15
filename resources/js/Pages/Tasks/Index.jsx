c11d14aeaf794a496d49d3d1ceb088a31a160303
Abdul Muqtadir <129877143+Muqtadir44@users.noreply.github.com>
2025-06-15 14:32:58 +0500

    adding delete in task

--- resources/js/Pages/Tasks/Index.jsx
+++ resources/js/Pages/Tasks/Index.jsx
@@ -1,11 +1,12 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link} from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 import TaskTable from "./TaskTable";
 import { BreadCrumb } from "@/Components/BreadCrumb";
 import PageHeading from "@/Components/PageHeading";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import 'toastr/build/toastr.min.css';
 import toastr from 'toastr';
+import { DeleteModal } from "@/Components/DeleteModal";
 
 export default function Index({ tasks, queryParams = null }) {
 
@@ -21,7 +22,16 @@ export default function Index({ tasks, queryParams = null }) {
         }
     }, [flash]);
 
-   return (
+
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [deleteId, setDeleteId] = useState(null);
+
+    const deleteTask = (id) => {
+        setDeleteId(id);
+        setShowDeleteModal(true);
+    }
+
+    return (
         <AuthenticatedLayout
             header={
                 <PageHeading title={'Tasks'} btnTitle={'New Task'} addBtn={true} addRoute={'tasks.create'} />
@@ -38,10 +48,16 @@ export default function Index({ tasks, queryParams = null }) {
                     </BreadCrumb>
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="pb-6 text-gray-900 dark:text-gray-100">
-                            <TaskTable tasks={tasks} queryParams={queryParams} />
+                            <TaskTable tasks={tasks} queryParams={queryParams} deleteTask={deleteTask} />
                         </div>
                     </div>
                 </div>
+                <DeleteModal
+                    show={showDeleteModal}
+                    deleteId={deleteId}
+                    deleteRoute={'tasks.destroy'}
+                    onClose={() => setShowDeleteModal(false)}
+                />
             </div>
         </AuthenticatedLayout>
     )
