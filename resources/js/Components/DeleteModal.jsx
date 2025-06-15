fb6aedbfae08b3621e3c6efeab59c73c13cda5c3
Abdul Muqtadir <129877143+Muqtadir44@users.noreply.github.com>
2025-06-15 14:26:50 +0500

    udpating delete msg

--- resources/js/Components/DeleteModal.jsx
+++ resources/js/Components/DeleteModal.jsx
@@ -19,7 +19,7 @@ export function DeleteModal({ show, deleteId, deleteRoute, onClose }) {
         <Modal show={show} onClose={onClose}>
             <div className="p-6">
                 <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">
-                    Are you sure you want to delete this user?
+                    Are you sure you want to delete this?
                 </h2>
                 <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
                     This action cannot be undone.
