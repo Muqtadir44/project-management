f0980da4fa63b447e65ecbc18aa8828029fd7171
Muqtadir44 <kalhoromuqtadir125@gmail.com>
2025-06-20 15:34:07 +0500

    dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
@@ -18,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth','verified'])->group(function(){
-    Route::get('/dashboard',fn()=> Inertia::render('Dashboard'))->name('dashboard');
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
 
     Route::resource('users',UserController::class);
     Route::resource('projects',ProjectController::class);
